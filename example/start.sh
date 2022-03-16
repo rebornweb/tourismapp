639d3cd9100f06d3d69cb1fd259bbb38c71ab69d
Andrei <andrei.nicolas@hotmail.com>
2022-03-16 20:43:38 +1300

    example ruby react app

---
+++ example/start.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+#Ref https://guides.rubyonrails.org/getting_started.html
+#Ref https://zayne.io/articles/how-to-build-a-crud-app-with-ruby-on-rails-and-react
+#rails new open-flights --webpack=react --database=postgresql
+#cd open-flights
+#gem install tzinfo-data
+#bundle update
+#bundle install
+#rails db:create
