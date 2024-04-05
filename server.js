3c289b7e0159dd42277a90c0ee99d828f283b122
Andrei <Andrei.nicolas@hotmail.com>
2024-04-05 13:32:46 +1300

    Port change

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080-;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
